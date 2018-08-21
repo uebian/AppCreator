54952b06bb92cb225b218c987374d4caeeb340e0
uebian <2452973529@qq.com>
2018-08-21 08:43:28 +0800

    修改包名

---
+++ app/src/main/java/net/creatersstudio/appcreater/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package net.creatersstudio.appcreater.activities;
+import android.support.v7.app.*;
+import android.os.*;
+
+public class MainActivity extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		// TODO: Implement this method
+		super.onCreate(savedInstanceState);
+	}
+	
+}
