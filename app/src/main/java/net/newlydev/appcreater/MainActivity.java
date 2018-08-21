1c3ae8a0f8186ee866a49d7aaaa11db3c9f0adda
newlydev <2452973529@qq.com>
2018-08-21 08:16:33 +0800

    初始化项目

---
+++ app/src/main/java/net/newlydev/appcreater/MainActivity.java
@@ -0,0 +1,14 @@
+package net.newlydev.appcreater;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
