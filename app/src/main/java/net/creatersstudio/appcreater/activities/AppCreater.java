24c88f9803df7f098947727fbec3d6dacb88084e
debatebug <1759761439@qq.com>
2018-08-22 17:15:57 +0800

    添加两个方法用于获取AppCreater在外部存储相关文件的文件夹

--- app/src/main/java/net/creatersstudio/appcreater/activities/AppCreater.java
+++ app/src/main/java/net/creatersstudio/appcreater/activities/AppCreater.java
@@ -1,6 +1,21 @@
 package net.creatersstudio.appcreater.activities;
 
 import android.app.Application;
+import android.os.Environment;
+
+import net.creatersstudio.appcreater.R;
+
+import java.io.File;
 
 public class AppCreater extends Application{
+
+    //获取本应用在外部存储Project的文件夹对象
+    public File getProjectsFolder(){
+        return new File(Environment.getExternalStorageDirectory() + File.separator + getString(R.string.ext_projects_folder_name));
+    }
+
+    //获取本应用在外部存储其他文件的文件夹对象
+    public File getAppExtFolder(){
+        return new File(Environment.getExternalStorageDirectory() + File.separator + getString(R.string.ext_app_folder_name));
+    }
 }
